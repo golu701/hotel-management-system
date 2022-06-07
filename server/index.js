05785740efe5ff032d555ad0725ea28c5128066c
golu701 <golurajak811@gmail.com>
2022-06-07 22:11:55 +0530

    New: .example.env file, basic setup of express project, hotel model added Update: packages and scripts added for express project

--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./router/auth.js";
+import hotelRoute from "./router/hotel.js";
+import roomRoute from "./router/room.js";
+import userRoute from "./router/user.js";
 const app = express();
 dotenv.config()
 
 try {
-    await mongoose.connect("mongodb://localhost:27017/hotel-management-system")
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Database connected");
 }
 catch (error) {
-    handleError(error)
+    throw error;
+    // handleError(error)
 }
+
+mongoose.connection.on("disconnect", () => {
+    console.log("Database disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Mongodb connected");
+})
+
+
+// routes
+app.use("/auth", authRoute);
+app.use("/hotel", hotelRoute);
+app.use("/room", roomRoute);
+app.use("/user", userRoute);
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ "message": "this is business" })
+})
 
 app.listen(8000, () => {
     console.log()
